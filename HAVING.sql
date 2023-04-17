09315e1333c2258e8c254fba060486b5439dc7b3
Andzej Krupoves <andrejkrupovec@gmail.com>
2023-04-17 12:35:21 +0300

    HAVING clause only after GROPU BY or if an aggregate fn was used

--- HAVING.sql
+++ HAVING.sql
@@ -2,5 +2,14 @@
    WHERE ~ like but only what was aggregated through GROUP BY!
 */
 
+SELECT customer_id, SUM(amount) FROM payment
+GROUP BY customer_id
+HAVING SUM(amount)>200
+ORDER BY SUM(amount) DESC
+
+SELECT staff_id,customer_id, SUM(amount) FROM payment
+WHERE staff_id=2
+GROUP BY staff_id,customer_id
+HAVING SUM(amount)>100
 
 
