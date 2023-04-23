6b88f2ecf7ac8d8ef1301ddda0ecdf98a1c2ba55
Andzej Krupoves <andrejkrupovec@gmail.com>
2023-04-23 10:19:15 +0300

    CASE real world examples

--- CASE.sql
+++ CASE.sql
@@ -43,6 +43,27 @@ SELECT a,
     END
 FROM test_tbl
     
+-- Real world example    
+SELECT customer_id,first_name,last_name,-- CASE statement's col
+CASE customer_id -- we're checking some values againts just this column 
+	WHEN 2 THEN 'Winner'
+	WHEN 5 THEN '2nd place'
+	ELSE 'Normal'
+END AS raffle_res
+FROM customer
 
-
+SELECT  -- we're going to have 3 columns, since we have 3 CASE statements
+SUM(CASE rental_rate 
+	WHEN 0.99 THEN 1 -- counting rows that are equal to 0.99 and return this col #1
+	ELSE 0
+END) as bargains,
+SUM (CASE rental_rate -- counting rows that are equal to 2.99 and return this col #2
+	WHEN 2.99 THEN 1
+	ELSE 0
+END) AS regular,
+SUM (CASE rental_rate -- counting rows that are equal to 4.99 and return this col #3
+	WHEN 4.99 THEN 1
+	ELSE 0
+END) AS premium
+FROM film
 
